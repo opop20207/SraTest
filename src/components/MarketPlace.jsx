1486cb63e151327017c44475b1b38c876300fab0
rudan <ruddd8256@naver.com>
2022-04-11 02:02:02 +0900

    마켈 플레이스 더미데이터 부르기

--- src/components/MarketPlace.jsx
+++ src/components/MarketPlace.jsx
@@ -1,8 +1,53 @@
+import Item from './MarketPlace/Item'
+
+
+function Loaditems() {
+
+    var items=[
+        {
+        img: "http://placeimg.com/340/280/people/sepia",
+        title: "더미 타이틀",
+        price: "더미 가격",
+        goods: "더미 좋아요"
+        },
+        {
+            img: "http://placeimg.com/340/280/people/sepia",
+            title: "더미 타이틀",
+            price: "더미 가격",
+            goods: "더미 좋아요"
+        },
+        {
+            img: "http://placeimg.com/340/280/people/sepia",
+            title: "더미 타이틀",
+            price: "더미 가격",
+            goods: "더미 좋아요"
+        },
+        {
+            img: "http://placeimg.com/340/280/people/sepia",
+            title: "더미 타이틀",
+            price: "더미 가격",
+            goods: "더미 좋아요"
+        }
+
+    ]
+
+   
+
+    return items
+}
 
 function MarketPlace(){
+   
+
+    var products=Loaditems();
 
     return(
+        <>  
+ 
         <p>마켓입니다dsadas</p>
+        <Item products={products}/>
+        </>
+      
     );
 }
 
