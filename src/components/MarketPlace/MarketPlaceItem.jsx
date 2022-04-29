fd50739d6e70b2f6c3f14fb90e4fecafd0402318
rudan <ruddd8256@naver.com>
2022-04-29 11:29:26 +0900

    카드수정

--- src/components/MarketPlace/MarketPlaceItem.jsx
+++ src/components/MarketPlace/MarketPlaceItem.jsx
@@ -19,16 +19,24 @@ function MarketPlaceItem(props) {
         props.products.map((product) => (
             <div key={product.id} className="card">
                 <NavLink to={`/MarketPlace/${product.id}`}>
-                    <img className="imgcontent" src={product.imageURI} />
-                </NavLink>
-                <div id="content">
-                    <p id="title">{product.name}</p>
-                    <div id="aligncontent">
-                        <p id="description">{product.description}</p>
+
+                    <div  className="imgcontent">
+                    <img src={product.imageURI} />
                     </div>
-                    <p id="temp">가격</p>
-                    <p id="price">{product.price}</p>
-                </div>
+                    
+
+                    <div id="cardContent">
+                        <p id="title">{product.name}</p>
+                        <div id="aligncontent">
+                            <p id="description">{product.description}</p>
+                        </div>
+                        <p id="temp">가격</p>
+                        <p id="price">{product.price}</p>
+                            
+                    </div>
+                   
+        
+                </NavLink>
             </div>
         ));
 
