4c32c72ba2a7c2e578e0ba51d4def798bf6fb468
geongu kang <kdr06006@naver.com>
2022-05-17 17:16:33 +0900

    skin 2d -> 3d 기능 완성

--- src/hooks/SkinView3d.jsx
+++ src/hooks/SkinView3d.jsx
@@ -1,11 +1,26 @@
 import * as skinview from "skinview3d";
 import {useEffect} from 'react';
+import reactImageSize from 'react-image-size';
 
 function SkinView3d({ imgLink }) {
     let skinViewer;
     let rotateAnimation;
 
-  function initializeViewer() {
+    const isAble2Dto3D = async() => {
+      try {
+        const {width, height} = await reactImageSize(imgLink);
+        console.log(width + " and " + height);
+        return width == 64 && height == 64;
+      } catch {
+        console.log("fail to load image");
+        return false;
+      }
+    }
+
+  const initializeViewer = async() => {
+    const isAble = await isAble2Dto3D();
+    if (!isAble) return;
+
     skinViewer = new skinview.FXAASkinViewer({
       canvas: document.getElementById(imgLink),
     });
@@ -17,12 +32,20 @@ function SkinView3d({ imgLink }) {
   }
 
   const onMouse = () => {
+    if(!skinViewer) {
+      console.log("onMouse : " + "return!");
+      return;
+    }
     rotateAnimation = skinViewer.animations.add(
         skinview.RotatingAnimation
     );
   }
 
   const outMouse = () => {
+    if(!rotateAnimation) {
+      console.log("outMouse : " + "return!");
+      return;
+    }
       rotateAnimation.resetAndRemove();
       rotateAnimation = null;
   }
