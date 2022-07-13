f8aa4026c1fc8f2a9c9d507b7742cac15b80cd36
geongu kang <kdr06006@naver.com>
2022-07-13 13:35:09 +0900

    middleware (2)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
     app.use(
         createProxyMiddleware('/api' , {
             //target: 'http://clonens.com:3000',
-            //target: 'https://ipfs.moralis.io:2053',
-            target: 'http:localhost:3000',
+            target: 'https://ipfs.moralis.io:2053',
+            //target: 'http:localhost:3000',
             changeOrigin : true
         })
     );
