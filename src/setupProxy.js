12ce36b9b0787ea6cfc1651aabf49333e56cfea6
geongu kang <kdr06006@naver.com>
2022-07-14 15:00:35 +0900

    ipfs testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware('/api' , {
-            //target: 'http://clonens.com:3000',
-            target: 'https://ipfs.moralis.io:2053',
-            //target: 'http:localhost:3000',
+            target: 'http://clonens.com:3000',
+            //target: 'https://ipfs.moralis.io:2053',
+            //target: 'http://localhost:3000',
             changeOrigin : true
         })
     );
