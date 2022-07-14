5371a5fdc1861f211246bbdb900b7685c27cc3b5
geongu kang <kdr06006@naver.com>
2022-07-14 16:55:18 +0900

    memory update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+c
 module.exports = function(app) {
     app.use(
-        createProxyMiddleware('/api' , {
+        createProxyMiddleware('/ipfs' , {
             target: 'http://clonens.com:3000',
             //target: 'https://ipfs.moralis.io:2053',
             //target: 'http://localhost:3000',
