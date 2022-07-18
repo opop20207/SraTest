9d85b8c1811c5914309482d44884ed6c51c328a6
jsy <opop20207@naver.com>
2022-07-18 14:23:29 +0900

    yes2

--- api/[value].js
+++ api/[value].js
@@ -1,9 +1,8 @@
 import axios from "axios";
-
+import { useState } from "react";
 export default async function handler(req, res) {
     const imageURI = req.query;
-    try {
-    } catch {}
+
     res.json({
         imageBase64: axios
             .get(imageURI, {
