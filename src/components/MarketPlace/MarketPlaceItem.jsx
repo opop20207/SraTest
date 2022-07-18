f79218b7c9a90b24100dc3a78e8dc0bb4f22082c
jsy <opop20207@naver.com>
2022-07-18 16:06:34 +0900

    a123123

--- src/components/MarketPlace/MarketPlaceItem.jsx
+++ src/components/MarketPlace/MarketPlaceItem.jsx
@@ -21,7 +21,7 @@ async function MarketPlaceItem(props) {
                     <div className="imgContent">
                         {console.log(
                             product.imageURI,
-                            axios.get("/api/ipfs", {
+                            axios.get("/api/test", {
                                 params: { imageURI: product.imageURI },
                             })
                         )}
